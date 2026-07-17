670d3363723291e5b78b1cba238dc120dd9c7867
mohits-25 <mohitsmohit650@gmail.com>
2026-07-17 16:13:45 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (31.81%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,55 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-        int[] ans = new int[2];
-
-        int lb = lowerbound(nums, n, target);
-
-        if (lb == n || nums[lb] != target) {
-            return new int[]{-1, -1};
-        }
-
-        int ub = upperbound(nums, n, target) - 1;
-
-        ans[0] = lb;
-        ans[1] = ub;
-
-        return ans;
+        int n=nums.length;
+        int a[]=new int[2];
+        a[0]=first(nums,n,target);
+        a[1]=last(nums,n,target);
+        return a;
     }
-
-    public int lowerbound(int[] arr, int n, int x) {
-        int low = 0, high = n - 1;
-        int ans = n;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (arr[mid] >= x) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+    public int first(int arr[],int n,int k){
+        int low=0;
+        int high=n-1;
+        int first=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==k){
+                first=mid;
+                high=mid-1;
             }
+            else if(arr[mid]<k) low=mid+1;
+            else high=mid-1;
         }
-
-        return ans;
+        return first;
     }
-
-    public int upperbound(int[] arr, int n, int x) {
-        int low = 0, high = n - 1;
-        int ans = n;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (arr[mid] > x) {  
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+    public int last(int arr[],int n,int k){
+        int low=0;
+        int high=n-1;
+        int last=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==k){
+                last=mid;
+                low=mid+1;
             }
+            else if(arr[mid]<k) low=mid+1;
+            else high=mid-1;
         }
-
-        return ans;
+        return last;
     }
 }
